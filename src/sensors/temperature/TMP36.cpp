20c09a65904c716a80164957168b87ef95e84e7e
Jeff Niu <jeffniu22@gmail.com>
2018-02-08 10:53:05 -0500

    Cleaned up some sensor code

--- src/sensors/temperature/TMP36.cpp
+++ src/sensors/temperature/TMP36.cpp
@@ -3,19 +3,20 @@
 using namespace wlp;
 
 TMP36::TMP36(Board::AnalogPin analog_pin)
-    : pin(analog_pin) {}
+    : m_analog_pin(analog_pin) {}
 
 double TMP36::read_temperature() {
-    double reading = pin.sample();
-    double voltage = reading * SCALE;
-    float temperature_celsius = (voltage - 0.5) * 100.0; //converting from 10 mv per degree with 500 mV offset to degrees
+    uint16_t reading = m_analog_pin.sample();
+    double voltage = static_cast<double>(reading) * SCALE;
+    // Converting from 10 mV per degree Celsius with 500 mV offset to degrees
+    double temperature_celsius = (voltage - 0.5) * 100.0;
     return temperature_celsius;
 }
 
 void TMP36::begin() {
-    pin.powerup();
+    m_analog_pin.powerup();
 }
 
 void TMP36::sleep() {
-    pin.powerdown();
+    m_analog_pin.powerdown();
 }
