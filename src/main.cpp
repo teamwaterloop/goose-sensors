012c0a27b614d6c72496655f7f2d7f7a9a3d8999
Josie Eshkenazi <eshkenazi.josephine@gmail.com>
2018-04-11 19:32:17 -0400

    some changes (#10)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <Cosa/Trace.hh>
 #include <Cosa/UART.hh>
+#include "sensors/kinematic/LSM9DS1.h"
+
+wlp::LSM9DS1 lsm;
+
 
 void setup() {
     uart.begin(9600);
     trace.begin(&uart);
     trace << "======== GOOSE 3 SENSORS ========" << endl;
+
+    if(lsm.begin()) {
+        trace <<"Start succeeded" << endl;
+    } else {
+        trace <<"Start failed" << endl;
+    }
 }
 
 void loop() {
+    if(lsm.accel_available()){
+        trace <<"Accel ready" << endl;
+        lsm.accel_read();
+    }else {
+        trace<< "Accel not ready" << endl;
+    }
+    trace <<lsm.accel_x() << ", " << lsm.accel_y() <<", " << lsm.accel_z() << endl;
     delay(1000);
+
 }
